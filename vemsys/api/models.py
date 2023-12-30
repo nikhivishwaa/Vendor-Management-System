874af7ea54187d854441b136835c40fe5b60d96a
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-30 06:07:23 +0530

    perform_create method overrider for VendorViewSet Class

--- vemsys/api/models.py
+++ vemsys/api/models.py
@@ -1,6 +1,35 @@
 from typing import Any
 from django.db import models
+from random import randint, choice
 
+
+# Generate unique string code of 10 digit
+class VendorCode():
+    id_counter: int = 999
+
+    def __init__(self):
+        self.name = self.value()
+        VendorCode.id_counter += 1
+
+    def value(self) -> str:
+        zerovalue = ['A', 'B', 'C', 'X', 'Y', 'Z']
+        mainstr = ""
+        count = VendorCode.id_counter
+        for i in range(8):
+            x = (ord('C') + count % 20)
+            count //= 20
+            if x == ord('C') or x >= ord('X'):
+                mainstr += choice(zerovalue)
+            else:
+                mainstr += chr(x)
+        A, Z = ord('A'), ord('Z')
+        mainstr += chr(randint(A, Z))
+        mainstr += chr(randint(A, Z))
+        return mainstr[::-1]
+
+    def __str__(self) -> str:
+        return self.name
+    
 
 # Model for vendors
 class Vendor(models.Model):
@@ -12,7 +41,6 @@ class Vendor(models.Model):
         primary_key=True,
         null=False,
         editable=False,
-        db_index=True,
     )
     on_time_delivery_rate = models.FloatField()
     quality_rating_avg = models.FloatField()
@@ -21,3 +49,10 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.name
+    
+    # def __init__(self, *args: Any, **kwargs: Any) -> None:
+    #     super().__init__(*args, **kwargs)
+    #     x = VendorCode()
+    #     self.vendor_code = x.name
+
+
--- vemsys/api/views.py
+++ vemsys/api/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import Vendor
+from .models import Vendor, VendorCode
 from .serializers import VendorSerializer
 from rest_framework.response import Response
-from .unique_code import UniqueString
 
 
 # Create your views here.
 class VendorViewSet(ModelViewSet):
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
+
     def perform_create(self, serializer):
-        id = UniqueString()
-        serializer.vendor_code = id.name
+        id = VendorCode()
+        serializer.vendor_code = id.value()
         serializer.save()
+
+    
